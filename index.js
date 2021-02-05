5fa78a720c1d75eed54deeadd5c5044d544a44e6
Andrew Koshelev <ndrei61999@gmail.com>
2021-02-05 12:29:45 -1000

    Init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '1675308278:AAEYrIw2-rTwuxbGcS-BybSo8ESmneRO2Vg';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Получили твое сообщение! Спасибо!');
+});
