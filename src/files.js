17ee55ac100560e7352f702810598eceded9a8b6
Andrew Koshelev <ndrei61999@gmail.com>
2021-02-09 23:37:13 -1000

    External links for files

---
+++ src/files.js
@@ -0,0 +1,21 @@
+module.exports = {
+    // game
+    calendarPic1: 'https://sun9-3.userapi.com/impf/9bnoBwrT-M-M-WAMZXBpn7-mnW7t7mdByzFz_Q/gkOBm2nu5z4.jpg?size=1280x852&quality=96&proxy=1&sign=ef00fb957e41677af08414b22f32faa9&type=album',
+    // Prayer
+    calendarPic2: 'https://sun9-51.userapi.com/impf/aEtrt6aeHPM1y5qSF3EzEzDS81dX9Rc302Ar_w/_kfUJEx9z5U.jpg?size=1080x1080&quality=96&proxy=1&sign=f4871801a45271b5885026cfdd9a1ef4&type=album',
+    // Exit
+    calendarPic3: 'https://sun9-44.userapi.com/impf/xEiGK5r-3_b73XX2AmKh7laQE-2zDV0tVIMv6w/t7S6UNoi--Y.jpg?size=1080x1080&quality=96&proxy=1&sign=02fded5853eb7b5bbbb28d5e9342ed86&type=album',
+    calendarPic4: '',
+    calendarPic5: '',
+
+    mapUrl: 'https://yandex.ru/maps/?um=constructor%3A35dad1dc46caf371e7d494ce0a91041adc121c5fde390dd1c7217bd6faee2a13&source=constructorLink',
+
+    // materials for small groups
+    materialsForSG: {
+        danBook: './materials/Сила Малой группы.pdf',
+        msgHelp: {
+            doc1: './materials/Советы для ведущих.docx',
+            doc2: './materials/Ценности МГ.docx',
+        },
+    },
+}
