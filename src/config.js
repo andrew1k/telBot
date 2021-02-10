08ec3e4ced2495399ac4b51adc5ad73221bedd47
Andrew Koshelev <ndrei61999@gmail.com>
2021-02-10 03:22:44 -1000

    Revoke Api token

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '1675308278:AAEYrIw2-rTwuxbGcS-BybSo8ESmneRO2Vg',
+    token: '1675308278:AAFv0mQ53UEzcXOb121gBko-JoPouc3K91c',
     idAdmin: '639954422',
     // DB_URL: 'mongodb://localhost/telBot'
     
