7c86bab63a400e05bf9637300c2b4530f82720d8
Andrew Koshelev <ndrei61999@gmail.com>
2021-02-09 23:31:14 -1000

    Remove all Unnecessary coments from index

---
+++ src/commented.js
@@ -0,0 +1,54 @@
+// All from index.js
+
+// DB ==========================
+// const mongoose = require('mongoose')
+
+// mongoose.connect(config.DB_URL).then(() => {console.log('DB Successfully added...');})
+//                                .catch((err) => {console.log(err);})
+
+// const Schema = mongoose.Schema
+
+// const userScheme = new Schema({
+//     userName: String,
+//     UserText: String,
+//     formFrom: String,
+// });
+
+// const UserReply = mongoose.model('UserReply', userScheme)
+// DB ==========================
+
+
+
+
+
+
+
+// let SGL = [] // Small Group Leader
+// let SGM = [] // Small Group Member
+// let FAQ = [] // Frequently Asked Questions
+// let SERVUN = [] // Serv Unknown
+// let PRAYFORM = [] // Pray form
+// let SERVHI = [] // Serv Hire
+// let OPOS = [] // One Plus One Student
+// let OPOM = [] // Ono Plus One Mentor
+
+
+
+
+
+// .then(() => {console.log('All Right');})
+// .catch((err) => {console.log(err);})
+
+
+
+
+
+// if (msg.text !== kb.smallGroups.sgLeader && msg.text !== kb.smallGroups.sgMap && msg.text !== kb.smallGroups.goHome) {
+//     bot.on('message', msg => {
+//         bot.forwardMessage(idAdmin, chatId, msg.message_id)
+//         fs.writeFileSync('./data.txt', helper.debug(`Сообщение от вкладки 'Хочу стать участником Малой Группы' от ${msg.from.username}, текст: ${msg.text}`))
+//         console.log(helper.debug(`Сообщение из 'Хочу стать участником Малой Группы' от ${msg.from.username}, текст: ${msg.text}`))
+//     })
+// }
+
+
