7507d1c711a657107e016e846449383f6fa8fb0c
Andrew Koshelev <ndrei61999@gmail.com>
2021-02-10 05:50:00 -1000

    Revoke token

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '1675308278:AAFv0mQ53UEzcXOb121gBko-JoPouc3K91c',
+    token: '1403842472:AAEXRhVODx037xem0U9BMG4l9gRvz0KTh08',
     idAdmin: '639954422',
     // DB_URL: 'mongodb://localhost/telBot'
     
