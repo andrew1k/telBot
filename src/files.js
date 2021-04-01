a3704e1b77831046098a1bf7d6d0861b4636b1ca
Andrew Koshelev <ndrei61999@gmail.com>
2021-04-01 06:06:21 -1000

    Update Pics

--- src/files.js
+++ src/files.js
@@ -1,14 +1,18 @@
 module.exports = {
     // game
-    calendarPic1: 'https://sun9-24.userapi.com/impf/walWraBNNHPgC524k1wt-55KBZ6MSwW3gkEulw/LV_Klo_jmzE.jpg?size=2560x1920&quality=96&proxy=1&sign=6d87ef65bad2eaca8995d27631392a66&type=album',
+    calendarPic1: 'https://sun9-76.userapi.com/impf/cKP3st7ukw50J0bxOgGH3w3EsqAR1Vf5S1PbFg/Lw0cI906hfA.jpg?size=1280x1280&quality=96&sign=e981494dc59ec1e3f0bbc79e061e8380&type=album',
     // Prayer
-    calendarPic2: 'https://sun9-51.userapi.com/impf/aEtrt6aeHPM1y5qSF3EzEzDS81dX9Rc302Ar_w/_kfUJEx9z5U.jpg?size=1080x1080&quality=96&proxy=1&sign=f4871801a45271b5885026cfdd9a1ef4&type=album',
+    calendarPic2: 'https://psv4.userapi.com/c536236/u38543685/docs/d27/91ba9727d706/Molitva.png?extra=Une9otM32KIq253fuG84L3UyDU7pyW5I7JT8fMKb5f4EXwE5W-_TtlMPzUXzf_QFM15BK4zPAPjKpqYOKuEc8bBfK7FBthGWm2WQQI41fJ77W7EpzufmUvNw0M4K2CVJbFlkFVNBdpGpS0ariJlb5O8X',
     // Exit
-    calendarPic3: 'https://img.redbull.com/images/q_auto,f_auto/redbullcom/2018/10/23/f2456748-8797-467e-b8e3-f829e62bef7f/jumping-at-thin-air-photo-3',
+    calendarPic3: 'https://www.openbusiness.ru/upload/iblock/42e/bowl.jpg',
     // Tea 
     calendarPic4: 'https://sun9-9.userapi.com/impf/tGtXPzIAF9FhwA1GBhNypAge0-UH4H-yNtzw5Q/di1hQKHE4c0.jpg?size=1080x1348&quality=96&proxy=1&sign=a539b4ceb24fee03cf5e2c10d236b224&type=album',
     // Steps
     calendarPic5: 'https://mbv.spb.ru/wp-content/uploads/2016/08/cover-book2-1024x853-1.jpg',
+    // DomashkaParty
+    calendarPicDomashkaParty: 'https://sun9-44.userapi.com/impf/dSDpbmWSMjvIE7nchC8Wkqhqx6ZhGeRH6Wottg/Ho0pX7rZn4w.jpg?size=1527x2160&quality=96&sign=5ae1bbdaab79ccdaca48700c454d3751&type=album',
+    // calendarforTokShow
+    calendarPicForTokShow: 'https://psv4.userapi.com/c536236/u38543685/docs/d49/0b66315f57b9/Tok-Shou.png?extra=DXh95Kh-1iM9x_kw6CzxZqeasPUdHVGq6NFuRoYtXEoPWPA_HucaUJgOZJsHbQ_Zq6dvsBN0uvlaRfnEAyq5SH-rYyCeAcf63383e47Dk57FkN_G2yalaCigoyjYsJ3OUCsE2G-QBvwcbmOSjY1SW2ox',
 
     // About Church
     aboutChurch: 'https://sun9-45.userapi.com/impf/NGL4Y9rLUa0vlFdXXG5rp1YvB2Y8KcXNoBZRIA/1KJqRfX2NZU.jpg?size=1131x1600&quality=96&sign=41e077f28a3cbdbe241b34570daaf17b&type=album',
