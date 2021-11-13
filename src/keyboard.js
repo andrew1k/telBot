b2c092ad090d96a09d582dead686c3e118d28324
Andy <ndrei61999@gmail.com>
2021-11-13 21:01:27 +0300

    Keyboard update

--- src/keyboard.js
+++ src/keyboard.js
@@ -12,10 +12,10 @@ module.exports = {
     ],
     calendar: [
         [calendar.calendarKeyboard.item1],
+        [calendar.calendarKeyboard.item5],
         [calendar.calendarKeyboard.item6],
-        // [calendar.calendarKeyboard.item3],
-        [calendar.calendarKeyboard.item4],
-        // [calendar.calendarKeyboard.item5],
+        [calendar.calendarKeyboard.item3],
+        // [calendar.calendarKeyboard.item4],
         // [calendar.calendarKeyboard.item8],
         // [calendar.calendarKeyboard.item7],
         [calendar.calendarKeyboard.item2],
